40a21d624c95e90dca4c6289b1b44f300f074ac5
Alewi Ahmed <alewimail@gmail.com>
2018-04-26 16:09:53 +0300

    created a boilerplate for the timer.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,26 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return <div className="App">empty project</div>;
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="row">
+            <div>
+              <p className="adjusting-text">BREAK LENGHT</p>
+              <p>- 5 +</p>
+            </div>
+            <div>
+              <p className="adjusting-text">SESSION LENGHT</p>
+              <p>- 25 +</p>
+            </div>
+          </div>
+          <div className="timer">
+            <p className="timer-text">Session</p>
+            <p className="timer-text">time</p>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
